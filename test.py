4947f0fa9ca53d921386962ed447805b1ec9d533
Khuong Duy <killer123578910@gmail.com>
2026-08-04 21:31:51 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+# import matplotlib as plt
+import pandas as pd
+
+# class player:
+    
+    
+
+def corefunc(choi):
+    ke=np.random.default_rng()
+    dices=np.array(ke.integers(low=1,high=6,size=3))
+    print(dices.sum())
+    
+    
+if __name__=="__main__":
+    playerchoice=int(input("nhap 1 or 2: "))
+    corefunc(playerchoice)
